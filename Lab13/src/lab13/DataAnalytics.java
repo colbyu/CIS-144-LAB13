005842d650965bfe2cecc6d0efe41bec7cc56828
colby <colby@10.1.10.3>
2016-12-06 12:17:57 -0600

    Made copy-paste modifications as requested

--- Lab13/src/lab13/DataAnalytics.java
+++ Lab13/src/lab13/DataAnalytics.java
@@ -54,6 +54,12 @@ public static void main(String args[])
     fin.close();
     fileIn.close();
 
+    System.out.println("display unsorted data");
+    			DisplayData(list);
+
+     System.out.println("display sorted data");
+    			BubbleSort(list);
+          
   }
   catch(Exception e)
   {
@@ -61,4 +67,31 @@ public static void main(String args[])
     System.err.println("Error: " + e.getMessage());
   }
 }
+public static void DisplayData(ArrayList<Integer> num)
+		{
+			for (int i = 0; i < num.size(); i++)  
+       		 System.out.println(num.get(i).toString());
+ 		}
+public static void BubbleSort(ArrayList<Integer> num)
+    {
+    int j = 0;
+    boolean flag = true;  // set the flag to true to begin first pass
+    int temp = 0;  // define the holding variable
+    while (flag)
+        {
+        flag = false; //set flag to false awaiting a possible swap
+        for (j = 0; j < num.size() - 1; j++)
+            {
+            if (num.get(j) > num.get(j + 1)) 
+            // for descending sort change to <
+                {
+                temp = num.get(j); //swap the elements
+                num.set(j, num.get(j + 1));
+                num.set(j + 1, temp);
+                flag = true; //shows a swap occurred  
+                } 
+            } 
+        }
+    } 
+
 }
