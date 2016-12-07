c38a5cc17e0a4fb7280ebeed816f275e7a6e519e
colby <colby@10.1.10.3>
2016-12-06 19:02:45 -0600

    Made copy-paste modifications as requested

--- Lab13/src/lab13/DataAnalytics.java
+++ Lab13/src/lab13/DataAnalytics.java
@@ -1,107 +1,92 @@
 
 package lab13;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
-// Sammy Student: Data Analysis with Java File Processing
 
-class DataAnalytics
-{
-public static void main(String args[])
-{
-  // declare an object to receive the data
-  Scanner scan = new Scanner(System.in);
-  // declare an array list to hold the data
-  ArrayList<Integer> list;
-  list = new ArrayList<Integer>();
-  int count = 0;
-  int num = 0;
-  int val = 0;
-  String line = "";
-  int[] sortedNumbers = new int[15];
+//Colby Underhill: Data Analysis with Java File Processing
 
-  try
-  {
-    // create or append to the file 
-    FileWriter fileOut = new FileWriter("outData.txt");
-    BufferedWriter fout = new BufferedWriter(fileOut);
-    
-    System.out.println("how many data items?");
-    count = scan.nextInt();
-    for (int i = 1; i <= count; i++)
-    {
-      System.out.println("enter a data value");
-      val = scan.nextInt();
-      fout.write(val + "\n");
-    }
-    System.out.println("thank you ... the data has been recorded!");
-    
-    // close the output stream objects
-    fout.close();
-    fileOut.close();
+class DataAnalytics {
 
-    scan.close();
-    // read the data
-    FileReader fileIn = new FileReader("outData.txt");
-    BufferedReader fin = new BufferedReader(fileIn);
-    
-    while ((line = fin.readLine()) != null)
-    {
-     	num = Integer.parseInt(line);
-    	list.add(num);
-      System.out.println(num);
-    }
-    System.out.println("thank you ... the data has been received!");
-    fin.close();
-    fileIn.close();
+    public static void main(String args[]) {
+        // declare an object to receive the data
+        Scanner scan = new Scanner(System.in);
+        // declare an array list to hold the data
+        ArrayList<Integer> list;
+        list = new ArrayList<Integer>();
+        int count = 0;
+        int num = 0;
+        int val = 0;
+        String line = "";
 
-    System.out.println("display unsorted data");
-    			DisplayData(list);
+        try {
+            // create or append to the file 
+            FileWriter fileOut = new FileWriter("outData.txt");
+            BufferedWriter fout = new BufferedWriter(fileOut);
 
-     System.out.println("display sorted data");
-    		sortedNumbers = BubbleSort ();
+            System.out.println("how many data items?");
+            count = scan.nextInt();
+            for (int i = 1; i <= count; i++) {
+                System.out.println("enter a data value");
+                val = scan.nextInt();
+                fout.write(val + "\n");
+            }
+            System.out.println("thank you ... the data has been recorded!");
 
-    for (int i=0; i < sortedNumbers.length; i++)
-    {
-        System.out.println("The sorted numbers are: ");
-        System.out.print(sortedNumbers[i]+ " ");
-    }
-  }
-  catch(Exception e)
-  {
-    // catch an exception if any arises
-    System.err.println("Error: " + e.getMessage());
-  }
-}
-public static void DisplayData(ArrayList<Integer> num)
-		{
-			for (int i = 0; i < num.size(); i++)  
-       		 System.out.println(num.get(i).toString());
- 		}
-public static void BubbleSort(ArrayList<Integer> num)
-    {
-    int j = 0;
-    boolean flag = true;  // set the flag to true to begin first pass
-    int temp = 0;  // define the holding variable
-    while (flag)
-        {
-        flag = false; //set flag to false awaiting a possible swap
-        for (j = 0; j < num.size() - 1; j++)
-            {
-            if (num.get(j) > num.get(j + 1)) 
-            // for descending sort change to <
-                {
-                temp = num.get(j); //swap the elements
-                num.set(j, num.get(j + 1));
-                num.set(j + 1, temp);
-                flag = true; //shows a swap occurred  
-                } 
-                    
-            } 
+            // close the output stream objects
+            fout.close();
+            fileOut.close();
 
+            scan.close();
+            // read the data
+            FileReader fileIn = new FileReader("outData.txt");
+            BufferedReader fin = new BufferedReader(fileIn);
+
+            while ((line = fin.readLine()) != null) {
+                num = Integer.parseInt(line);
+                list.add(num);
+                System.out.println(num);
+            }
+            System.out.println("thank you ... the data has been received!");
+            fin.close();
+            fileIn.close();
+
+            System.out.println("display unsorted data");
+            DisplayData(list);
+
+            System.out.println("display sorted data");
+            BubbleSort(list);
+
+        } catch (Exception e) {
+            // catch an exception if any arises
+            System.err.println("Error: " + e.getMessage());
         }
+    }
 
-      
-    } 
+    public static void DisplayData(ArrayList<Integer> num) {
+        for (int i = 0; i < num.size(); i++) {
+            System.out.println(num.get(i).toString());
+        }
+    }
 
+    public static void BubbleSort(ArrayList<Integer> num) {
+
+        int j = 0;
+        boolean flag = true;  // set the flag to true to begin first pass
+        int temp = 0;  // define the holding variable
+        while (flag) {
+            flag = false; //set flag to false awaiting a possible swap
+            for (j = 0; j < num.size() - 1; j++) {
+                if (num.get(j) > num.get(j + 1)) // for descending sort change to <
+                {
+                    temp = num.get(j); //swap the elements
+                    num.set(j, num.get(j + 1));
+                    num.set(j + 1, temp);
+                    flag = true; //shows a swap occurred  
+                }
+            }
+        }
+        System.out.println(num.toString());
+    }
 }
