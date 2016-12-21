b7ac54fe506bb69ee429d29da48a4f11778a3aa8
colby <colby@10.1.10.3>
2016-12-21 17:22:08 -0600

    Made copy-paste modifications as requested

--- Lab13/src/lab13/DataAnalytics.java
+++ Lab13/src/lab13/DataAnalytics.java
@@ -1,15 +1,15 @@
-
 package lab13;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
-
 //Colby Underhill: Data Analysis with Java File Processing
 
-class DataAnalytics {
-static int min = 0, max = 0;
-    public static void main(String args[]) {
+class DataAnalytics 
+    {
+    static int min = 0, max = 0;
+    public static void main(String args[]) 
+        {
         // declare an object to receive the data
         Scanner scan = new Scanner(System.in);
         // declare an array list to hold the data
@@ -19,95 +19,89 @@ static int min = 0, max = 0;
         int num = 0;
         int val = 0;
         String line = "";
-
-        try {
+        try 
+            {
             // create or append to the file 
             FileWriter fileOut = new FileWriter("outData.txt");
             BufferedWriter fout = new BufferedWriter(fileOut);
-
             System.out.println("how many data items?");
             count = scan.nextInt();
-            for (int i = 1; i <= count; i++) {
+            for (int i = 1; i <= count; i++) 
+                {
                 System.out.println("enter a data value");
                 val = scan.nextInt();
                 fout.write(val + "\n");
-            }
+                }
             System.out.println("thank you ... the data has been recorded!");
-
             // close the output stream objects
             fout.close();
             fileOut.close();
-
             scan.close();
             // read the data
             FileReader fileIn = new FileReader("outData.txt");
             BufferedReader fin = new BufferedReader(fileIn);
-
-            while ((line = fin.readLine()) != null) {
+            while ((line = fin.readLine()) != null) 
+                {
                 num = Integer.parseInt(line);
                 list.add(num);
                 System.out.println(num);
-            }
+                }
             System.out.println("thank you ... the data has been received!");
             fin.close();
             fileIn.close();
-
             System.out.println("display unsorted data");
             DisplayData(list);
-
-            BubbleSort(list);
-            
+            BubbleSort(list);//Sort the data!!!
             System.out.println("display sorted data");
             DisplayData(list);
-
-            System.out.println("MinMax = " + min + max);
-            
-        } catch (Exception e) {
+            Calculations(list);  //display max,min values
+            //Average(list); //display average values
+            } 
+        catch (Exception e) 
+            {
             // catch an exception if any arises
             System.err.println("Error: " + e.getMessage());
+            }
         }
-    }
-
-    public static void DisplayData(ArrayList<Integer> num) {
-        for (int i = 0; i < num.size(); i++) {
+    public static void DisplayData(ArrayList<Integer> num) 
+        {
+        for (int i = 0; i < num.size(); i++) 
+            {
             System.out.println(num.get(i).toString());
+            }
         }
-    }
-
-    public static void BubbleSort(ArrayList<Integer> num) {
-
+    public static void BubbleSort(ArrayList<Integer> num) 
+        {
         int j = 0;
         boolean flag = true;  // set the flag to true to begin first pass
         int temp = 0;  // define the holding variable
-        while (flag) {
+        while (flag) 
+            {
             flag = false; //set flag to false awaiting a possible swap
-            for (j = 0; j < num.size() - 1; j++) {
-                if (num.get(j) > num.get(j + 1)) // for descending sort change to <
+            for (j = 0; j < num.size() - 1; j++) 
                 {
+                if (num.get(j) > num.get(j + 1)) // for descending sort change to <
+                    {
                     temp = num.get(j); //swap the elements
                     num.set(j, num.get(j + 1));
                     num.set(j + 1, temp);
                     flag = true; //shows a swap occurred  
+                    }
                 }
             }
         }
-        //System.out.println(num.toString());
-    }
-public static void MinMax(ArrayList<Integer> num)
+public static void Calculations(ArrayList<Integer> num)
     {
-    int min = num.get(0);
-    int max = num.get(0);
-
-    for(Integer i: num) {
-        if(i < min) min = i;
-        if(i > max) max = i;
-
-    System.out.println("min = " + min);
-    System.out.println("max = " + max);
-    }
-    }
-    public static void Average(ArrayList<Integer> num)
-    {
-        
+    double sum = 0, min = 0, max = 0;
+    min = num.get(0);
+    max = num.get(num.size()-1);
+    for (int i = 0; i<num.size();++i)
+        {
+        sum += num.get(i);
+        }
+    System.out.println("min value intered = " + min);
+    System.out.println("max value entered = " + max);
+    System.out.println("total of the values = " + sum);
+    System.out.println("average of the values = " + sum/num.size());
     }
 }
